01616ae5344ad2fec79fc1287b29cf3c7ab555b3
hujiaming <jih189@ucsd.edu>
2021-11-12 13:33:59 -0800

    first clean up

--- manipulation/regrasp/examples/nxtgooff.py
+++
@@ -1,6 +0,0 @@
-import robotcon.nextage as nxtcon
-
-if __name__=='__main__':
-    nxts = nxtcon.NxtSocket()
-    nxts.initialize()
-    nxts.gooff()
--- robotsim/rostest/rossubtext.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from std_msgs.msg import String
-
-def callback(message):
-    rospy.loginfo("I heard %s", message.data)
-
-rospy.init_node("listener")
-pub = rospy.Subscriber("chatter", String, callback)
-rospy.spin()
